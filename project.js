80a4f149df22d40af3655aa94bc2ce3d4cea7285
Allantoizmughus <allanmugweru22@gmail.com>
2021-03-15 00:56:02 +0300

    create form 3

--- project.js
+++ project.js
@@ -34,6 +34,137 @@ function product(){
     document.querySelector("#result").innerHTML=calculate;
 }
 
+function product1(){
+    var i =parseInt(document.querySelector("#epd1").value);
+    var j =parseInt(document.querySelector("#dpm1").value);
+    var op =document.querySelector("#operator1").value;
+    var calculate1;
+    if(op == "multiply"){
+        calculate1=i*j;
+    }
+    document.querySelector("#total1").innerHTML=calculate1;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product3(){
+    var m =parseInt(document.querySelector("#epd3").value);
+    var n =parseInt(document.querySelector("#dpm3").value);
+    var op =document.querySelector("#operator3").value;
+    var calculate3;
+    if(op == "multiply"){
+        calculate3=m*n;
+    }
+    document.querySelector("#total3").innerHTML=calculate3;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
+
+function product2(){
+    var k =parseInt(document.querySelector("#epd2").value);
+    var l =parseInt(document.querySelector("#dpm2").value);
+    var op =document.querySelector("#operator2").value;
+    var calculate2;
+    if(op == "multiply"){
+        calculate2=k*l;
+    }
+    document.querySelector("#total2").innerHTML=calculate2;
+}
 
 
 
