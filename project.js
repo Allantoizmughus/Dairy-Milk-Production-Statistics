6d43b9cb4ffb73a603113be59010f633846d0cd0
Allantoizmughus <allanmugweru22@gmail.com>
2021-03-14 13:01:17 +0300

    creat a form in the body

---
+++ project.js
@@ -0,0 +1,13 @@
+function calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var c = parseInt(document.querySelector("#value3").value);
+    var d = parseInt(document.querySelector("#value4").value);
+    var op =document.querySelector("#operator").value;
+    var calculate;
+    if (op == "add"){
+      calculate = a+b+c+d;
+    }
+    document.querySelector("#results").innerHTML=calculate;
+}
+
